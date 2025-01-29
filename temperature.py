5e2298b236a7d2c9d6d419bf23bb4ca969561185
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2025-01-29 13:51:18 +0200

    Wrote code that converts from Celsius to fahrenheit and from fahrenheit to celsius

---
+++ temperature.py
@@ -0,0 +1,10 @@
+temperature = input("Would you like to convert from Celsius or Fahrenheit? ")
+
+if temperature == "celsius":
+    celsius = int(input("Enter your temperature in celsius: "))
+    fahrenheit = (celsius * 9/5) + 32
+    print(f"{fahrenheit}°F")
+elif temperature == "fahrenheit":
+    fahrenheit = int(input("Enter your temperature in fahrenheit: "))
+    celsius = (fahrenheit - 32) * 5/9
+    print(f"{celsius}°C")
